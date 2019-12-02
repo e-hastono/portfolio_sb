881093d943069ac2cf1ecdb3b2117a4a3333230b
Emmanoel Hastono <emmanoel.h@bgrindonesia.co.id>
2019-12-02 14:59:52 +0700

    Updates

---
+++ precache-manifest.54b402c3b5bfa37f537a0e3c4893884b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d6ef06c0c79ebe5f14d2f8f1954960",
+    "url": "/portfolio_sb/index.html"
+  },
+  {
+    "revision": "be3aeba67b05d1bddf68",
+    "url": "/portfolio_sb/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6d75676371c098510b64",
+    "url": "/portfolio_sb/static/css/main.f34387d7.chunk.css"
+  },
+  {
+    "revision": "be3aeba67b05d1bddf68",
+    "url": "/portfolio_sb/static/js/2.c53400e4.chunk.js"
+  },
+  {
+    "revision": "6d75676371c098510b64",
+    "url": "/portfolio_sb/static/js/main.6db2a66f.chunk.js"
+  },
+  {
+    "revision": "bf04020246d2bf670662",
+    "url": "/portfolio_sb/static/js/runtime-main.8365e171.js"
+  },
+  {
+    "revision": "cba429d9358689b7c8d187ea54cef76f",
+    "url": "/portfolio_sb/static/media/profile.cba429d9.jpg"
+  }
+]);
